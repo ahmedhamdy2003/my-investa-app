662b985be922719cbd91922d9a15101e2953bfe5
Ahmed Khaled <ahmedbardesy95@gmail.com>
2025-06-09 16:10:12 +0300

    create founder dashboard main apis functions.

---
+++ lib/core/founder_api/get_founder_profit_margin_trend.dart
@@ -0,0 +1,79 @@
+import 'dart:convert';
+
+import 'package:investa4/core/utils/api_services.dart';
+import 'package:investa4/core/utils/global_variables.dart';
+import 'package:investa4/core/utils/manage_current_user.dart';
+import 'package:investa4/core/widgets/show_snackbar.dart';
+
+Future<DounderProfitMarginTrend?> getFounderProfitMarginTrend() async {
+  String url =
+      '$baseUrl/founder/dashboard/profit-margin-trend/${ManageCurrentUser.currentUser.guid}/';
+  // late Reel reell;
+  try {
+    var response = await ApiClient.getData(url);
+    if (response != null) {
+      showDebugSnackBar(response.toString(), navigatorKey.currentContext!);
+      try {
+        if (response.containsKey('error')) {
+          showDebugSnackBar(response['error'], navigatorKey.currentContext!);
+          return null;
+        }
+        if (response is Map) {
+          showDebugSnackBar(
+            'Category percentages fetched',
+            navigatorKey.currentContext!,
+          );
+          return DounderProfitMarginTrend.fromMap(response);
+        } else if (response is String) {
+          Map<String, dynamic> categoryPercentages = {};
+          try {
+            // Attempt to parse the string as JSON
+            categoryPercentages = Map<String, dynamic>.from(
+              jsonDecode(response) as Map,
+            );
+            showDebugSnackBar(
+              'Category percentages fetched from string',
+              navigatorKey.currentContext!,
+            );
+            return DounderProfitMarginTrend.fromMap(categoryPercentages);
+          } catch (e) {
+            showDebugSnackBar(
+              'Error parsing JSON: $e',
+              navigatorKey.currentContext!,
+            );
+            return null;
+          }
+        } else {
+          showDebugSnackBar(
+            'Unexpected response format',
+            navigatorKey.currentContext!,
+          );
+          return null;
+        }
+      } catch (e) {
+        showDebugSnackBar(
+          '### requestOtp error: $e',
+          navigatorKey.currentContext!,
+        );
+
+        return null;
+      }
+    } else {
+      showDebugSnackBar('requestOtp null', navigatorKey.currentContext!);
+      return null;
+    }
+  } catch (e) {
+    showDebugSnackBar('requestOtp error: $e', navigatorKey.currentContext!);
+    return null;
+  }
+}
+
+class DounderProfitMarginTrend {
+  final dynamic data;
+
+  DounderProfitMarginTrend({required this.data});
+
+  factory DounderProfitMarginTrend.fromMap(Map<dynamic, dynamic> map) {
+    return DounderProfitMarginTrend(data: map['data'] ?? []);
+  }
+}
--- lib/core/utils/global_variables.dart
+++ lib/core/utils/global_variables.dart
@@ -6,3 +6,35 @@ final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 String baseUrl = 'https://decc-102-190-139-157.ngrok-free.app';  
 
 
+
+// founder
+// https://bd69-41-44-28-57.ngrok-free.app/founder/dashboard/overview/4iVtZAbBbVXDdOdzhSniVDPJK5a2/
+
+// {
+//     "total_invested_amount": 0,
+//     "number_of_investors": 0,
+//     "revenue": 0,
+//     "realized_profit": 0
+// }
+
+
+
+// https://bd69-41-44-28-57.ngrok-free.app/founder/dashboard/return-vs-comparison/4iVtZAbBbVXDdOdzhSniVDPJK5a2/
+
+// {
+//     "investment_return": {},
+//     "comparison_data": {}
+// }
+
+
+
+// https://bd69-41-44-28-57.ngrok-free.app/founder/dashboard/portfolio-performance/4iVtZAbBbVXDdOdzhSniVDPJK5a2/
+
+// {}
+
+
+
+// https://bd69-41-44-28-57.ngrok-free.app/founder/dashboard/profit-margin-trend/4iVtZAbBbVXDdOdzhSniVDPJK5a2/
+
+// {}
+
